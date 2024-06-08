8909c6c58743a6be673ea98aed0d2f7b330d0ade
hardik200404 <Polenati.hardik@gmail.com>
2024-06-08 15:51:55 +0530

    Arrow function and object added

--- first-app.js
+++ first-app.js
@@ -1,1 +1,19 @@
 console.log("hello world")
+
+//arrow function
+let product_of_two=(a,b)=>{
+    return a*b
+}
+
+console.log(product_of_two(2,4))
+
+//student object with greet function
+let student={
+    name:'Hardik',
+    age:26,
+    greet(){
+        console.log("Hello I'm ",this.name)
+    }
+}
+
+student.greet()
