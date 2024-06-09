09613d7a86a230a49931e10227b6eff78c6cbd60
hardik200404 <Polenati.hardik@gmail.com>
2024-06-09 19:21:46 +0530

    added "/add-product" route with form in server.js

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 const express=require('express');
 const app=express();
 
+const body_parser=require('body-parser')
+app.use(express.urlencoded({ extended: true }))
+
 let port=4000
 
-//middleware
-app.use(function(req,res,next){
-    console.log("In middleware")
-    next()
+//home
+app.get('/',function(req,res,next){
+    res.send("Hello World")
 })
 
-//home
-app.use(function(req,res,next){
-    res.send("Hello World")
+app.get('/add-product',(req,res)=>{
+    let html=`
+    <!DOCTYPE html>
+    <html>
+    <head><title>enter message</title></head>
+    <body>
+        <form action='/added' method='POST'>
+            <input type='text' name='product'></input>
+            <input type='text' name='size'></input>
+            <button type='submit'>Submit</button>
+        </form>
+    </body>
+    </html>
+    `
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(html)
+    return res.end()
+})
+
+app.post('/added',(req,res)=>{
+    console.log("'",req.body.product,"' Added as Product")
+    console.log("With Size: ",req.body.size)
+    res.statusCode=302
+    res.setHeader('Location','/add-product')
+    return res.end()
 })
 
 app.listen(port,()=>{
