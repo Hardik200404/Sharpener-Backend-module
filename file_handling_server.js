88ba921aec9c5fe4b90e6947177466f524a27a45
hardik200404 <Polenati.hardik@gmail.com>
2024-06-09 13:10:54 +0530

    file handling server added

---
+++ file_handling_server.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+let http=require('http')
+
+let server=http.createServer((req,res)=>{
+    let url=req.url
+    let method=req.method
+
+    if(url==='/'){
+        let body=fs.readFileSync('message.txt',{ encoding: 'utf8', flag: 'r' })
+        
+        let html=`
+            <!DOCTYPE html>
+            <html>
+            <head><title>enter message</title></head>
+            <body>
+                <p>${body}</p>
+                <form action='/message' method='POST'>
+                    <input type='text' name='message'></input>
+                    <button type='submit'>Submit</button>
+                </form>
+            </body>
+            </html>
+            `
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(html)
+        return res.end()
+    }
+    if(url==='/message' && method==='POST'){
+        let body=[]
+        req.on('data',(chunk)=>{
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            let parsed_message=Buffer.concat(body).toString()
+            let message=parsed_message.split('=')[1]
+            console.log(message)
+            fs.writeFileSync('message.txt',message)
+        })
+        res.statusCode=302
+        res.setHeader('Location','/')
+        return res.end()
+    }
+
+    res.end('Hello')
+})
+
+server.listen(4000)
+
