ed671116b8aadf18ecde4f65aff9d46bc10ce501
hardik200404 <Polenati.hardik@gmail.com>
2024-06-10 18:22:41 +0530

    Chat app done

--- simple_chat_app/routes/chat_route.js
+++ simple_chat_app/routes/chat_route.js
@@ -2,10 +2,7 @@ let fs=require('fs')
 let path=require('path')
 module.exports=function(app){
     app.get('/',(req,res)=>{
-        res.sendFile(path.join(__dirname,'../','/views/message.html'))
-    }),
-    app.get('/chats',(req,res)=>{
-        let body,html
+        let body
         if(fs.existsSync('./chat.txt')){
             body=fs.readFileSync('./chat.txt',{ encoding: 'utf8', flag: 'r' })
         }else{
@@ -13,36 +10,14 @@ module.exports=function(app){
         }
         res.render('../views/chat',{content:body})
     }),
-
-        // html=`
-        // <!DOCTYPE html>
-        // <html>
-        // <head><title>enter message</title></head>
-        // <body>
-        //     <p>${body}</p>
-        //     <form action='/' method='POST'>
-        //         <input type='text' name='message' placeholder='Enter Message'></input>
-        //         <button type='submit'>Submit</button>
-        //     </form>
-        // </body>
-        // <header>
-        //     <nav>
-        //         <ul>
-        //             <li><a href="/login">Login</a></li>
-        //         </ul>
-        //     </nav>
-        // </header>
-        // </html>
-        // `
-        // res.writeHead(200, { 'Content-Type': 'text/html' });
-        // res.write(html)
-        // return res.end()
     app.post('/submit-form',(req,res)=>{
         //console.log(req.body)
+        let username=req.body.username
         let message=req.body.message
-        fs.writeFileSync('./chat.txt',message)
+        let chat_message=username+": "+message+`\n`
+        fs.writeFileSync('./chat.txt',chat_message,{flag: 'a'})
         res.statusCode=302
-        res.setHeader('Location','/chats')
+        res.setHeader('Location','/')
         return res.end()  
     })
 }
